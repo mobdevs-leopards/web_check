60d7cab06a596ea1d49fbf79506c99f8ee6f89f6
Sheikh Muhammad Sabih <muhammad.sabih@leopardscourier.com>
2022-10-05 17:50:27 +0500

    sfsdf

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -35,9 +35,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "ac7897d527f7d0fe22d81d1fdaf8fb72",
-"/": "ac7897d527f7d0fe22d81d1fdaf8fb72",
-"main.dart.js": "75b63285de081b99ff35de2c89f1194e",
+"index.html": "8fee34ed4e4bce6a5603e84711be9dea",
+"/": "8fee34ed4e4bce6a5603e84711be9dea",
+"main.dart.js": "d5dabdc6a4edbde966dde2ebf11864dc",
 "manifest.json": "e1b7b7bb92c67f77581b661b5e002a92",
 "version.json": "57ae6f2f31d561013c144b65d5e7177a"
 };
